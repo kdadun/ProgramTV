dce9b894512b70a1db80c44a0cd6f2ad63f423a4
Kamil <kamil.dadun@interia.pl>
2017-09-20 01:27:04 +0200

    ProgramTV

---
+++ Agility2/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Agility2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.lblProgramTV = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.btnLoadProgram = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "TVP1",
+            "TVP2",
+            "TVN",
+            "TVN7",
+            "TVN Turbo",
+            "TTV",
+            "Polsat",
+            "Canal +"});
+            this.comboBox1.Location = new System.Drawing.Point(189, 22);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(129, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // lblProgramTV
+            // 
+            this.lblProgramTV.AutoSize = true;
+            this.lblProgramTV.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblProgramTV.Location = new System.Drawing.Point(123, 120);
+            this.lblProgramTV.Name = "lblProgramTV";
+            this.lblProgramTV.Size = new System.Drawing.Size(0, 16);
+            this.lblProgramTV.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(26, 152);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(556, 231);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Dzisiaj",
+            "Jutro",
+            "Pojutrze"});
+            this.comboBox2.Location = new System.Drawing.Point(189, 62);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(129, 21);
+            this.comboBox2.TabIndex = 5;
+            // 
+            // btnLoadProgram
+            // 
+            this.btnLoadProgram.Location = new System.Drawing.Point(353, 47);
+            this.btnLoadProgram.Name = "btnLoadProgram";
+            this.btnLoadProgram.Size = new System.Drawing.Size(100, 36);
+            this.btnLoadProgram.TabIndex = 6;
+            this.btnLoadProgram.Text = "Pokaż program";
+            this.btnLoadProgram.UseVisualStyleBackColor = true;
+            this.btnLoadProgram.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(333, 189);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 16);
+            this.label1.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(94, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(94, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(667, 395);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnLoadProgram);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.lblProgramTV);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Program TV";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label lblProgramTV;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button btnLoadProgram;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Agility2/Form1.cs
@@ -0,0 +1,255 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Agility2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label2.Text = "Wybierz telewizję";
+            label3.Text = "Wybierz dzień";
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            LoadProgram();
+        }
+
+        public void LoadProgram()
+        {
+            string tvp1 = "tvp-1-hd-380?dzien=0";
+            string polsat = "polsat-38?dzien=0";
+            string tvn = "tvn-357?dzien=0";
+            string tvp2 = "tvp-2-hd-145?dzien=0";
+            string tvn7 = "tvn-7-326?dzien=0";
+            string tvnTurbo = "tvn-turbo-hd-143?dzien=0";
+            string canalPlus = "canal-hd-288?dzien=0";
+            string ttv = "ttv-33?dzien=0";
+
+            List<string> program = new List<string>();
+            List<string> hour = new List<string>();
+            var html = (String)null;
+
+            HtmlWeb web = new HtmlWeb();
+            try
+            {
+                if (comboBox1.SelectedItem.ToString() == "TVP1")
+                {
+                    if (comboBox2.SelectedItem.ToString() == "Dzisiaj")
+                    {
+                        html = @"https://programtv.onet.pl/program-tv/" + tvp1.ToString();
+                        lblProgramTV.Text = "Dzisiaj w telewizji TVP1";
+                    }
+                    else if (comboBox2.SelectedItem.ToString() == "Jutro")
+                    {
+                        string tvp1j = "tvp-1-hd-380?dzien=1";
+                        html = @"https://programtv.onet.pl/program-tv/" + tvp1j.ToString();
+                        lblProgramTV.Text = "Jutro w telewizji TVP1";
+                    }
+                    else if (comboBox2.SelectedItem.ToString() == "Pojutrze")
+                    {
+                        string tvp1j2 = "tvp-1-hd-380?dzien=2";
+                        html = @"https://programtv.onet.pl/program-tv/" + tvp1j2.ToString();
+                        lblProgramTV.Text = "Pojutrze w telewizji TVP1";
+                    }
+
+                }
+                else if (comboBox1.SelectedItem.ToString() == "Polsat")
+                {
+                    if (comboBox2.SelectedItem.ToString() == "Dzisiaj")
+                    {
+                        html = @"https://programtv.onet.pl/program-tv/" + polsat.ToString();
+                        lblProgramTV.Text = "Dzisiaj w telewizji Polsat";
+                    }
+                    else if (comboBox2.SelectedItem.ToString() == "Jutro")
+                    {
+                        string polsatJ = "polsat-38?dzien=1";
+                        html = @"https://programtv.onet.pl/program-tv/" + polsatJ.ToString();
+                        lblProgramTV.Text = "Jutro w telewizji Polsat";
+                    }
+                    else if (comboBox2.SelectedItem.ToString() == "Pojutrze")
+                    {
+                        string polsatJ2 = "polsat-38?dzien=2";
+                        html = @"https://programtv.onet.pl/program-tv/" + polsatJ2.ToString();
+                        lblProgramTV.Text = "Pojutrze w telewizji Polsat";
+                    }
+                }
+                else if (comboBox1.SelectedItem.ToString() == "TVN")
+                {
+                    if (comboBox2.SelectedItem.ToString() == "Dzisiaj")
+                    {
+                        html = @"https://programtv.onet.pl/program-tv/" + tvn.ToString();
+                        lblProgramTV.Text = "Dzisiaj w telewizji TVN";
+                    }
+                    else if (comboBox2.SelectedItem.ToString() == "Jutro")
+                    {
+                        string tvnJ = "tvn-357?dzien=1";
+                        html = @"https://programtv.onet.pl/program-tv/" + tvnJ.ToString();
+                        lblProgramTV.Text = "Jutro w telewizji TVN";
+                    }
+                    else if (comboBox2.SelectedItem.ToString() == "Pojutrze")
+                    {
+                        string tvnJ2 = "tvn-357?dzien=2";
+                        html = @"https://programtv.onet.pl/program-tv/" + tvnJ2.ToString();
+                        lblProgramTV.Text = "Pojutrze w telewizji TVN";
+                    }
+                }
+                else if (comboBox1.SelectedItem.ToString() == "TVP2")
+                {
+                    if (comboBox2.SelectedItem.ToString() == "Dzisiaj")
+                    {
+                        html = @"https://programtv.onet.pl/program-tv/" + tvp2.ToString();
+                        lblProgramTV.Text = "Dzisiaj w telewizji TVP2";
+                    }
+                    else if (comboBox2.SelectedItem.ToString() == "Jutro")
+                    {
+                        string tvp2J = "tvp-2-hd-145?dzien=1";
+                        html = @"https://programtv.onet.pl/program-tv/" + tvp2J.ToString();
+                        lblProgramTV.Text = "Jutro w telewizji TVP2";
+                    }
+                    else if (comboBox2.SelectedItem.ToString() == "Pojutrze")
+                    {
+                        string tvp2J = "tvp-2-hd-145?dzien=2";
+                        html = @"https://programtv.onet.pl/program-tv/" + tvp2J.ToString();
+                        lblProgramTV.Text = "Pojutrze telewizji TVP2";
+                    }
+                }
+                else if (comboBox1.SelectedItem.ToString() == "TVN7")
+                {
+                    if (comboBox2.SelectedItem.ToString() == "Dzisiaj")
+                    {
+                        html = @"https://programtv.onet.pl/program-tv/" + tvn7.ToString();
+                        lblProgramTV.Text = "Dzisiaj w telewizji TVN7";
+                    }
+                    else if (comboBox2.SelectedItem.ToString() == "Jutro")
+                    {
+                        string tvn7j = "tvn-7-326?dzien=1";
+                        html = @"https://programtv.onet.pl/program-tv/" + tvn7j.ToString();
+                        lblProgramTV.Text = "Jutro w telewizji TVN7";
+                    }
+                    else if (comboBox2.SelectedItem.ToString() == "Pojutrze")
+                    {
+                        string tvn7j2 = "tvn-7-326?dzien=2";
+                        html = @"https://programtv.onet.pl/program-tv/" + tvn7j2.ToString();
+                        lblProgramTV.Text = "Pojutrze w telewizji TVN7";
+                    }
+                }
+                else if (comboBox1.SelectedItem.ToString() == "TVN Turbo")
+                {
+                    if (comboBox2.SelectedItem.ToString() == "Dzisiaj")
+                    {
+
+                        html = @"https://programtv.onet.pl/program-tv/" + tvnTurbo.ToString();
+                        lblProgramTV.Text = "Program telewizji TVN Turbo";
+                    }
+                    else if (comboBox2.SelectedItem.ToString() == "Jutro")
+                    {
+                        string tvnTurboj = "tvn-turbo-hd-143?dzien=1";
+                        html = @"https://programtv.onet.pl/program-tv/" + tvnTurboj.ToString();
+                        lblProgramTV.Text = "Jutro w telewizji TVN Turbo";
+                    }
+                    else if (comboBox2.SelectedItem.ToString() == "Pojutrze")
+                    {
+                        string tvnTurboj2 = "tvn-turbo-hd-143?dzien=2";
+                        html = @"https://programtv.onet.pl/program-tv/" + tvnTurboj2.ToString();
+                        lblProgramTV.Text = "Pojutrze w telewizji TVN Turbo";
+                    }
+                }
+                else if (comboBox1.SelectedItem.ToString() == "Canal +")
+                {
+                    if (comboBox2.SelectedItem.ToString() == "Dzisiaj")
+                    {
+                        html = @"https://programtv.onet.pl/program-tv/" + canalPlus.ToString();
+                        lblProgramTV.Text = "Dzisiaj w telewizji Canal +";
+                    }
+                    else if (comboBox2.SelectedItem.ToString() == "Jutro")
+                    {
+                        string canalPlusj = "canal-hd-288?dzien=1";
+                        html = @"https://programtv.onet.pl/program-tv/" + canalPlusj.ToString();
+                        lblProgramTV.Text = "Jutro w telewizji Canal +";
+                    }
+                    else if (comboBox2.SelectedItem.ToString() == "Pojutrze")
+                    {
+                        string canalPlusj2 = "canal-hd-288?dzien=2";
+                        html = @"https://programtv.onet.pl/program-tv/" + canalPlusj2.ToString();
+                        lblProgramTV.Text = "Pojutrze w telewizji Canal +";
+                    }
+                }
+                else if (comboBox1.SelectedItem.ToString() == "TTV")
+                {
+                    if (comboBox2.SelectedItem.ToString() == "Dzisiaj")
+                    {
+                        html = @"https://programtv.onet.pl/program-tv/" + ttv.ToString();
+                        lblProgramTV.Text = "Dzisiaj w telewizji TTV";
+                    }
+                    else if (comboBox2.SelectedItem.ToString() == "Jutro")
+                    {
+                        string ttvj = "ttv-33?dzien=1";
+                        html = @"https://programtv.onet.pl/program-tv/" + ttvj.ToString();
+                        lblProgramTV.Text = "Jutro w telewizji TTV";
+                    }
+                    else if (comboBox2.SelectedItem.ToString() == "Pojutrze")
+                    {
+                        string ttvj2 = "ttv-33?dzien=2";
+                        html = @"https://programtv.onet.pl/program-tv/" + ttvj2.ToString();
+                        lblProgramTV.Text = "Pojutrze w w telewizji TTV";
+                    }
+                }
+           
+
+            var htmldoc = web.Load(html);
+            var findhours = htmldoc.DocumentNode.SelectNodes("//div[@class='emissions']//div[@class='hours']//span[@class='hour']");
+            var findtitle = htmldoc.DocumentNode.SelectNodes("//div[@class='emissions']//div[@class='titles']//a[@href]");
+            if (findhours != null && findtitle != null)
+            {
+                foreach (var y in findtitle)
+                {
+                    program.Add(y.InnerText);
+                }
+                foreach (var x in findhours)
+                {
+                    hour.Add(x.InnerText);
+                }
+            }
+            else
+            {
+                MessageBox.Show("node = null");
+            }
+
+            var programPolsat = hour.Zip(program, (h, p) => new { Hours = h, Program = p });
+
+            DataTable table = new DataTable();
+            table.Columns.Add("Godzina", typeof(string));
+            table.Columns.Add("Program", typeof(string));
+
+
+            foreach (var prog in programPolsat)
+            {
+                DataRow row = table.NewRow();
+                row[0] = prog.Hours.ToString();
+                row[1] = prog.Program.ToString();
+
+                table.Rows.Add(row);
+            }
+            dataGridView1.DataSource = table;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+       
+    }
+}
